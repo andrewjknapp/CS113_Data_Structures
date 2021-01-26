a59310facc7fc945321d330ca515f999b539ad72
Andrew Knapp <ajknapp2015@gmail.com>
2021-01-25 23:09:14 -0800

    Implemented FizzBuzz algorithm

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -1,4 +1,26 @@
 package lab1;
 
 public class FizzBuzz {
+    public FizzBuzz() { }
+
+    public static void printFizzBuzz(int start, int end) {
+        boolean fizz, buzz;
+
+        for (int num = start; num <= end; num++) {
+            System.out.print(num + " ");
+
+            fizz = num % 3 == 0;
+            buzz = num % 5 == 0;
+
+            if (fizz) {
+                System.out.print("Fizz");
+            }
+
+            if (buzz) {
+                System.out.print("Buzz");
+            }
+
+            System.out.println();
+        }
+    }
 }
