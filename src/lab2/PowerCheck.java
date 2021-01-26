840f2a7720b9c8f961fb1f43975676f18b49e197
Andrew Knapp <ajknapp2015@gmail.com>
2021-01-26 10:37:35 -0800

    Implemented algorithm to check if number is a power of two

--- src/lab2/PowerCheck.java
+++ src/lab2/PowerCheck.java
@@ -3,7 +3,13 @@ package lab2;
 public class PowerCheck {
     public PowerCheck() { }
 
-    public static boolean isPowerOfTwo() {
+    public static boolean isPowerOfTwo(int num) {
+        String binaryNum = Integer.toBinaryString(num);
+        for (int index = 1; index < binaryNum.length(); index++) {
+            if (binaryNum.charAt(index) != '0') {
+                return false;
+            }
+        }
         return true;
     }
 }
