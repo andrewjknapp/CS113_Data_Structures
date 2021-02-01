518fe99b9fe174c420ce9ea41a45a99d1b588aa5
Andrew Knapp <ajknapp2015@gmail.com>
2021-02-01 10:47:49 -0800

    Added check to return false if zero or below and added more test cases

--- src/lab2/PowerCheck.java
+++ src/lab2/PowerCheck.java
@@ -4,6 +4,8 @@ public class PowerCheck {
     public PowerCheck() { }
 
     public static boolean isPowerOfTwo(int num) {
+        if (num <= 0) return false;
+
         String binaryNum = Integer.toBinaryString(num);
         for (int index = 1; index < binaryNum.length(); index++) {
             if (binaryNum.charAt(index) != '0') {
--- src/lab2/PowerCheckTest.java
+++ src/lab2/PowerCheckTest.java
@@ -1,22 +1,24 @@
 package lab2;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PowerCheckTest {
-    @BeforeEach
-    public void setup() {
 
+    @Test
+    public void Should_Recognize_Powers_Of_Two() {
+        assertTrue(PowerCheck.isPowerOfTwo(1));
+        assertTrue(PowerCheck.isPowerOfTwo(2));
+        assertTrue(PowerCheck.isPowerOfTwo(4));
+        assertTrue(PowerCheck.isPowerOfTwo(128));
+        assertTrue(PowerCheck.isPowerOfTwo(262144));
     }
 
     @Test
-    public void isPowerOfTwoCheck() {
-        assertEquals(PowerCheck.isPowerOfTwo(2), true);
-        assertEquals(PowerCheck.isPowerOfTwo(4), true);
-        assertEquals(PowerCheck.isPowerOfTwo(128), true);
-        assertEquals(PowerCheck.isPowerOfTwo(3), false);
-        assertEquals(PowerCheck.isPowerOfTwo(51), false);
+    public void Should_Recognize_Non_Powers_Of_Two() {
+        assertFalse(PowerCheck.isPowerOfTwo(0));
+        assertFalse(PowerCheck.isPowerOfTwo(3));
+        assertFalse(PowerCheck.isPowerOfTwo(51));
     }
 }
